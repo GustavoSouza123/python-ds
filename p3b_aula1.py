8a86793fbfda61f12ace9981eb4a0ee8f0a910e9
Gustavo Souza <gustavoelia7@gmail.com>
2023-07-25 08:52:11 -0300

    aula 1

---
+++ p3b_aula1.py
@@ -0,0 +1,18 @@
+# função print
+print(123)
+print("Hello World!!!")
+print(1, 2, 3)
+print(4, 5, 6, sep="-")
+
+print()
+print(type("Python"))
+print(type(123))
+print(type(2.5))
+print(10 == 10)
+print(10 == 11)
+
+print()
+print(int('1') + 1)
+print(float('1.5') + 1.2)
+print(bool(' '))
+print(str(11) + 'b')
