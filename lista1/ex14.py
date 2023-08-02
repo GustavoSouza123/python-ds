7dad65f5463aef9d0bd69130a74399c6209cf1e1
Gustavo Souza <gustavoelia7@gmail.com>
2023-08-02 11:08:31 -0300

    fix ex14

--- lista1/ex14.py
+++ lista1/ex14.py
@@ -1,5 +1,6 @@
 # testar
 peso = int(input("Entre com o peso de peixes: "))
-excesso = peso - 50
+excesso = 0
+if peso > 50: excesso = peso - 50
 multa = 4 * excesso
 print(f"O peso de peixes é {peso} kg. O excesso foi {excesso} kg, logo o valor da multa é de R$ {multa}")
