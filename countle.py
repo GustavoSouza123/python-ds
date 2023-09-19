bfef4676323000ec326ed8a61d55d2edffde0cb8
Gustavo Souza <gustavoelia7@gmail.com>
2023-09-19 08:36:06 -0300

    countle puzzle

---
+++ countle.py
@@ -0,0 +1,33 @@
+""" algoritmo para resolver o quebra-cabeça do site countle.org """
+
+result = int(input("Entre com o resultado da expressão: "))
+nums = []
+for i in range(6):
+    nums.append(int(input(f"Entre com o {i+1}º número da expressão: ")))
+
+for cont in range(4):
+    if cont == 0:
+        for i in range(0, len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == result:
+                    print("Você acertou!")
+                    print(f"{nums[i]} + {nums[j]} = {result}")
+    elif cont == 1:
+        for i in range(0, len(nums)):
+            for j in range(i, len(nums)):
+                if nums[i] - nums[j] == result:
+                    print("Você acertou!")
+                    print(f"{nums[i]} - {nums[j]} = {result}")
+    elif cont == 2:
+        for i in range(0, len(nums)):
+            for j in range(i, len(nums)):
+                if nums[i] * nums[j] == result:
+                    print("Você acertou!")
+                    print(f"{nums[i]} * {nums[j]} = {result}")
+    elif cont == 3:
+        for i in range(0, len(nums)):
+            for j in range(i, len(nums)):
+                if nums[i] != 0 and nums[j] != 0:
+                    if nums[i] / nums[j] == result:
+                        print("Você acertou!")
+                        print(f"{nums[i]} / {nums[j]} = {result}")
